92309d996a3a1ae3b1cc5c2dcb23eb921108b89d
Oualidlaib <laiboualid2003@gmail.com>
2024-04-06 12:13:21 +0200

    add authentication system | add custom user model | add testSkin API

--- Skin-Cancer-Mobile-App/skin_cancer_app/models.py
+++ Skin-Cancer-Mobile-App/skin_cancer_app/models.py
@@ -1,66 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 # Create your models here.
 
 
-class MyAccountManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(username=username,
-                          email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_admin', True)
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(username, email, password, **extra_fields)
-
-
-class Account(AbstractBaseUser):
-    def get_profile_image_filepath(self, filename):
-        return f"profile_images/{self.pk}/profile_img.png"
-
-    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
-    username = models.CharField(max_length=30, unique=True)
-    date_joined = models.DateTimeField(
-        verbose_name="date_joined", auto_now_add=True)
-    last_login = models.DateTimeField(verbose_name="last_login", auto_now=True)
-    is_admin = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username", ]
-
-    objects = MyAccountManager()
-
-    def has_perm(self, perm, obj=None):
-        return self.is_admin
-
-    def has_module_perms(self, app_label):
-        return True
-
-    def get_profile_image(self):
-        return str(self.profile_image[self.profile_image.index(f'profile_images/{self.pk}/'):])
-
-
 def user_directory_path(instance, filename):
-    return r'images\{0}'.format(filename)
+    return 'images/{0}/'.format(filename)
 
 
 class SkinImage(models.Model):
-    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(
-        upload_to=user_directory_path, default='posts/default.jpg')
-    upload_time = models.DateTimeField(auto_now_add=True)
+        upload_to='images', default='posts/default.jpg')
--- Skin-Cancer-Mobile-App/skin_cancer_app/urls.py
+++ Skin-Cancer-Mobile-App/skin_cancer_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+
 app_name = 'skin_cancer_app'
 urlpatterns = [
     path('test/', views.testSkinCancer),
