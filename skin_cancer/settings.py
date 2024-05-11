5de0b5e9e54577c3a4c67b709ea7473f76c994f4
Oualidlaib <laiboualid2003@gmail.com>
2024-05-11 03:11:53 +0200

    update-v9

--- skin_cancer/settings.py
+++ skin_cancer/settings.py
@@ -141,6 +141,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
