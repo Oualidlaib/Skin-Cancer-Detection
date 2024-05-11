f1e1f56aeee66299cd1a2e303395ee612fd487a7
Oualidlaib <laiboualid2003@gmail.com>
2024-05-11 03:57:48 +0200

    update-v12

--- skin_cancer/settings.py
+++ skin_cancer/settings.py
@@ -152,12 +152,12 @@ TEMP = os.path.join(BASE_DIR, 'media_cdn/temp')
 
 MEDIA_URL = '/media/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
+DEFAULT_AUTO_FIELD = os.environ.get("DEFAULT_AUTO_FIELD")
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = 25
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'laiboualid2003@gmail.com'
-EMAIL_HOST_PASSWORD = 'taoq pguh xiph ctps'
-DEFAULT_FROM_EMAIL = 'Oualid <laiboualid2003@gmail.com>'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
