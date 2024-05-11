e23ff03fcd9308dd05ce99e78a27463e6daf2169
Oualidlaib <laiboualid2003@gmail.com>
2024-05-11 03:01:22 +0200

    update-v8

--- skin_cancer/urls.py
+++ skin_cancer/urls.py
@@ -16,8 +16,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
+urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
+urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
